66004722b6bcd9afbb3f8f1bc01fe4fcafae3f3f
zenonass <zenonasauzbikas@gmail.com>
2021-07-28 22:38:56 +0300

    Added styling and functionality to the project

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let myLibrary = [];
 const cardsContainer = document.querySelector('.cards-container');
 const addNewBook = document.querySelector('#add-new-book');
+const submitForm = document.querySelector('#submit-book-form');
+const submitButton = document.querySelector('#btn-submit');
+
+
+const author = document.querySelector('#author');
+const title = document.querySelector('#title');
+const pages = document.querySelector('#pages');
+const read = document.querySelector('.btn-read');
 
 function Book(author, title, pages, read) {
   this.author = author
@@ -9,48 +17,82 @@ function Book(author, title, pages, read) {
   this.read = read
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book)
+addNewBook.addEventListener('click', ()=> {
+  submitForm.style.display = 'inline-block';
+  cardsContainer.style.filter = 'blur(5px)';
+})
+
+read.addEventListener('click', ()=>{
+  if (read.textContent === "Read") {
+    read.style.backgroundColor = "#da9797";
+    read.textContent = "Not Read";
+  }
+  else {
+    read.style.backgroundColor = "#d3ffcd";
+    read.textContent = "Read";
+  }
+  
+})
+
+
+submitButton.addEventListener('click',(e) =>{
+  e.preventDefault()
+  const newBook = new Book(author.value, title.value,`Pages: ${pages.value}`, read.textContent);
+  myLibrary.push(newBook)
+  submitForm.style.display = 'none';
+  cardsContainer.style.filter = 'none';
+  resetFormValues()
+  createBooks()
+  console.log(myLibrary);
+})
+
+function resetFormValues(){
+  author.value = '';
+  title.value = '';
+  pages.value = '';
 }
 
-const bookOne = new Book('Steven Kingffffffffffffffff fffff hdfhdhdf', 'IT', 351, true);
-const bookTwo = new Book('Batman', 'cool', 351, true);
+function clearBooks(){
+  cardsContainer.innerHTML='';
+}
 
-addBookToLibrary(bookOne);
-addBookToLibrary(bookTwo);
-addBookToLibrary(bookOne);
-addBookToLibrary(bookTwo);
-addBookToLibrary(bookTwo);
-addBookToLibrary(bookOne);
+function createBooks(){
+    clearBooks()
+    myLibrary.forEach((book, index) => {
+      const newCard = document.createElement("div");
+      const author = document.createElement('div');
+      const title = document.createElement('div');
+      const pages = document.createElement('div');
+      const read = document.createElement('button');
+      const deleteCard = document.createElement('button');
 
-myLibrary.forEach(book => {
-    const newCard = document.createElement("div");
-    const author = document.createElement('div');
-    const title = document.createElement('div');
-    const pages = document.createElement('div');
-    const read = document.createElement('button');
-    const deleteCard = document.createElement('button');
-    newCard.style.width = "30vh";
-    newCard.style.height = "30vh";
-    newCard.style.border = "1px solid";
-    newCard.style.backgroundColor = "grey";
-    newCard.style.borderRadius = "5%";
-    author.textContent = book.author;
-    title.textContent = book.title;
-    pages.textContent = book.pages;
-    read.textContent = book.read;
-    deleteCard.textContent = "Delete";
-    author.classList.add("card-item")
-    title.classList.add("card-item")
-    pages.classList.add("card-item")
-    read.classList.add("card-item","btn-read")
-    deleteCard.classList.add("card-item", "btn-delete")
-    newCard.appendChild(author);
-    newCard.appendChild(title);
-    newCard.appendChild(pages);
-    newCard.appendChild(read);
-    newCard.appendChild(deleteCard);
-    newCard.classList.add("card")
-    cardsContainer.appendChild(newCard);
-});
+      author.textContent = book.author;
+      title.textContent = book.title;
+      pages.textContent = book.pages;
+      read.textContent = book.read;
+      deleteCard.textContent = "Delete";
 
+      deleteCard.value = index
+
+      author.classList.add("card-item")
+      title.classList.add("card-item")
+      pages.classList.add("card-item")
+      read.classList.add("card-item","btn-read")
+      deleteCard.classList.add("card-item", "btn-delete")
+      newCard.classList.add("card")
+
+      newCard.appendChild(author);
+      newCard.appendChild(title);
+      newCard.appendChild(pages);
+      newCard.appendChild(read);
+      newCard.appendChild(deleteCard);
+      cardsContainer.appendChild(newCard);
+  });
+  const deleteCardButtons = document.querySelectorAll('.btn-delete');
+      deleteCardButtons.forEach(deleteCardButton => {
+        deleteCardButton.addEventListener('click', ()=> {
+          deleteCardButton.parentElement.remove();
+          myLibrary.splice(deleteCardButton.value)
+          });
+        })
+};
